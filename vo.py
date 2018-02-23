61c6717d958b30f98cf8c8ec1715e02805ecd6ec
Guru Manoj <amgurumanoj@gmail.com>
2018-02-23 14:37:32 +0530

    42

--- vo.py
+++ vo.py
@@ -18,6 +18,3 @@ def kangaroo(x1, v1, x2, v2):
 x1, v1, x2, v2 = input().strip().split(' ')
 x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
 result = kangaroo(x1, v1, x2, v2)
-print(result)
-print(x1)
-print(x1)
